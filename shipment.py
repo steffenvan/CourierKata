f0296294a24d632e876eccbc6455dfbe88b181d0
Steffen Van <steffenvan@Steffens-MacBook-Pro.local>
2020-01-23 22:03:28 +0100

    Updated shipment class to contain separate fields for fast and normal delivery.

--- shipment.py
+++ shipment.py
@@ -1,16 +1,26 @@
 from parcel import Parcel
+from shipping_price_info import ShippingPriceInfo
 
 class Shipment: 
-    def __init__(self):
-        self.normal_orders = []
-        self.fast_orders = []
-        self.total_shipment_price = 0
-    
-        
-    def set_shipment_rate(self, parcels: List[Parcel], parcel_rates: Dict[ParcelType, rate], fast_delivery = False): 
-        for parcel in parcels:
-            parcel.set_price()
-            self.total_shipment_price += parcel.price
+    # parcels: List[Parcel], shipment_infos: Dict[str, ShippingPriceInfo]
+    def __init__(self, parcels, shipment_infos, is_fast_delivery = False):
+        self.parcels = parcels
+        self.shipment_price = 0
+        self.fast_shipment_price = 0
+        self.shipment_infos = shipment_infos
+        self.is_fast_delivery = is_fast_delivery
+        self.set_shipment_price()
 
-        if fast_delivery:
-            self.total_shipment_price *= 2 
+    def set_shipment_price(self): 
+        for parcel in self.parcels:
+            parcel.set_price(self.shipment_infos)
+            self.shipment_price += parcel.get_price()
+
+        if self.is_fast_delivery:
+            self.fast_shipment_price = self.shipment_price * 2 
+
+    def get_shipment_price(self):
+        return self.shipment_price
+    
+    def get_fast_shipment_price(self):
+        return self.fast_shipment_price
